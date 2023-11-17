1a4cbc218404fd4860bca63b6f0db4196d5b5963
dotnetbabu <ayush06092002@gmail.com>
2023-11-17 15:01:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Connecting the graph/connecting-the-graph.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class DisjointSet {
+    
+
+    public:
+        vector<int> parent, rank, size;
+        DisjointSet(int n){
+            parent.resize(n + 1);
+            rank.resize(n + 1, 0);
+            size.resize(n + 1, 1);
+            for(int i = 0; i <= n; i++)
+            {
+                parent[i] = i;
+            }
+        }
+
+        int findUPar(int node)
+        {
+            if(node == parent[node])
+                return node;
+            
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v)
+        {
+            int ulPar_u = findUPar(u);
+            int ulPar_v = findUPar(v);
+
+            if(ulPar_u == ulPar_v)
+                return;
+            if(rank[ulPar_u] < rank[ulPar_v])
+            {
+                parent[ulPar_u] = ulPar_v;
+            }
+            else if(rank[ulPar_v] < rank[ulPar_u])
+            {
+                parent[ulPar_v] = ulPar_u;
+            }
+            else
+            {
+                parent[ulPar_v] = ulPar_u;
+                rank[ulPar_u]++;
+            }
+        }
+
+        void unionBySize(int u, int v)
+        {
+            int ulPar_u = findUPar(u);
+            int ulPar_v = findUPar(v);
+
+            if(ulPar_u == ulPar_v)
+                return;
+            if(size[ulPar_u] < size[ulPar_v])
+            {
+                parent[ulPar_u] = ulPar_v;
+                size[ulPar_v] += size[ulPar_u];
+            }
+            else
+            {
+                parent[ulPar_v] = ulPar_u;
+                size[ulPar_u] += size[ulPar_v];
+            }
+        }
+};
+class Solution {
+  public:
+    int Solve(int n, vector<vector<int>>& edge) {
+        // code here
+        DisjointSet ds(n);
+        int extra = 0;
+        
+        for(auto it : edge)
+        {
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u) != ds.findUPar(v))
+            {
+                ds.unionBySize(u, v);
+            }
+            else
+            {
+                extra++;
+            }
+            
+        }
+        
+        unordered_set<int> st;
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(ds.parent[i] == i)
+                count++;
+        }
+
+        if(count - 1 <= extra)
+            return count - 1;
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 2; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution Obj;
+        cout << Obj.Solve(n, adj) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
