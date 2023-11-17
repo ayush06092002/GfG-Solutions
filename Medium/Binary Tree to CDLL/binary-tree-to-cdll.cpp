753560c55d59b3ff815032b43a7fb7b46438cbb8
dotnetbabu <ayush06092002@gmail.com>
2023-11-17 23:08:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Binary Tree to CDLL/binary-tree-to-cdll.cpp
@@ -0,0 +1,199 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+void displayCList(Node *head)
+{
+    Node *itr = head;
+    do
+    {
+        cout << itr->data <<" ";
+        itr = itr->right;
+    } while (head!=itr);
+    cout <<endl;
+    itr=itr->left;
+    head = itr;
+    do{
+        cout<<itr->data<<" ";
+        itr=itr->left;
+    }while(head!=itr);
+    cout<<endl;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+class Solution
+{
+    private:
+    void convertToCDLL(Node* root, Node*& head, Node*& tail) {
+        if (root == nullptr)
+            return;
+
+        // Recursively convert the left subtree
+        convertToCDLL(root->left, head, tail);
+
+        // Process the current node
+        if (tail == nullptr) {
+            // If tail is null, it means this is the first node
+            head = root;
+        } else {
+            // Make the current node the successor of the previous node
+            tail->right = root;
+            root->left = tail;
+        }
+
+        // Update the tail to the current node
+        tail = root;
+
+        // Recursively convert the right subtree
+        convertToCDLL(root->right, head, tail);
+    }
+  public:
+    //Function to convert binary tree into circular doubly linked list.
+    Node *bTreeToCList(Node *root)
+    {
+    //add code here.
+         if (root == nullptr)
+            return nullptr;
+
+        Node* head = nullptr;
+        Node* tail = nullptr;
+
+        convertToCDLL(root, head, tail);
+
+        // Make it circular
+        if (head != nullptr && tail != nullptr) {
+            head->left = tail;
+            tail->right = head;
+        }
+
+        return head;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution obj;
+        Node *head = obj.bTreeToCList(root);
+        displayCList(head);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+// } Driver Code Ends
