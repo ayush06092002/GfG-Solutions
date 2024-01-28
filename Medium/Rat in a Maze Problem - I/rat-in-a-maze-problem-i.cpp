5929f2267e7c69839e1549fc5ce2f00009ab109b
dotnetbabu <ayush06092002@gmail.com>
2024-01-28 23:41:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    void solve(int i, int j, vector<vector<int>> &m, int n, string s, vector<string> &ans)
+    {
+        if(i < 0 || j < 0 || i >= n || j >= n || m[i][j] == 0)
+            return;
+        
+        if(i == n - 1 && j == n - 1)
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        m[i][j] = 0;
+        solve(i + 1, j, m, n, s + 'D', ans);
+        solve(i - 1, j, m, n, s + 'U', ans);
+        solve(i, j + 1, m, n, s + 'R', ans);
+        solve(i, j - 1, m, n, s + 'L', ans);
+        
+        m[i][j] = 1;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        string s = "";
+        vector<string> ans;
+        solve(0, 0, m, n, s, ans);
+        return ans;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
