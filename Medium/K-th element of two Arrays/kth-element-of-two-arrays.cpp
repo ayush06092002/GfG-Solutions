060537bb20907ef810e4ae331f5143d01bf9aa5b
dotnetbabu <ayush06092002@gmail.com>
2024-05-26 17:00:36 +0530

    Updated Solution - GfG to GitHub

--- Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -6,34 +6,36 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int nums1[], int nums2[], int n1, int n2, int k)
+    int kthElement(int arr1[], int arr2[], int n1, int n2, int k)
     {
-        if(n1 > n2)
-            return kthElement(nums2, nums1, n2, n1, k);
-        int n = n1 + n2;
-        int low = max(0, k - n2), high = min(n1 ,k);
-        while(low <= high)
-        {
-            int cut1 = (low + high) / 2;
-            int cut2 = k - cut1;
-
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            if(l1 <= r2 && l2 <= r1)
-            {
-                return max(l1, l2);
-            }
-            else if(l1 > r2)
-                high = cut1 - 1;
-            else
-                low = cut1 + 1;
+        if(n1 > n2){
+            return kthElement(arr2, arr1, n2, n1, k);
         }
-
-        return 0;         
+        int left = k;
+        int low = max(0, k - n2), high = min(k, n1);
+        int n = n1 + n2;
+        
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = (mid1 - 1 >= 0) ? arr1[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 - 1 >= 0) ? arr2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? arr1[mid1] : INT_MAX;
+            int r2 = (mid2 < n2) ? arr2[mid2] : INT_MAX;
+            
+            if(l1 <= r2 && l2 <= r1){
+                return max(l1, l2);
+                
+            }
+            else if(l1 > r2){
+                high = mid1 - 1;
+            }
+            else if(l2 > r1){
+                low = mid1 + 1;
+            }
+        }
+        
+        return 0;        
     }
 };
 
