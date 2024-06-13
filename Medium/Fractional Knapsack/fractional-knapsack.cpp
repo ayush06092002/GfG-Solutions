caaee2f52e47420d563d7301640cbed7f124e653
dotnetbabu <ayush06092002@gmail.com>
2024-06-13 21:58:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,83 +1,40 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Item{
-    int value;
-    int weight;
-};
 
 
-// } Driver Code Ends
-//class implemented
-/*
-struct Item{
-    int value;
-    int weight;
-};
-*/
 
 
 class Solution
 {
-    public:
-    static bool comp(Item A, Item B)
-    {
-        double ratioA = (double) A.value / A.weight;
-        double ratioB = (double) B.value / B.weight;
-        
-        return ratioA > ratioB;
-    }
-    //Function to get the maximum total value in the knapsack
-    
-    double fractionalKnapsack(int W, Item arr[], int n)
-    {
-        sort(arr, arr + n, comp);
-        
-        double maxValue = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i].weight <= W)
-            {
-                W -= arr[i].weight;
-                maxValue += arr[i].value;
-            }
-            else
-            {
-                double frac = (double) W / arr[i].weight;
-                maxValue += frac * arr[i].value;
-                break;
-            }
-        }
-        
-        return maxValue;
-    }
-        
-};
-
-
-//{ Driver Code Starts.
-int main()
+ 
+//Function to get the maximum total value in the knapsack.
+doublefractionalKnapsack(intW, Itemarr[], intn)
 {
-	int t;
-	//taking testcases
-	cin>>t;
-	cout<<setprecision(6)<<fixed;
-	while(t--){
-	    //size of array and weight
-		int n, W;
-		cin>>n>>W;
-		
-		Item arr[n];
-		//value and weight of each item
-		for(int i=0;i<n;i++){
-			cin>>arr[i].value>>arr[i].weight;
-		}
-		
-		//function call
-		Solution ob;
-		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
-	}
-    return 0;
+ 
+//1. sort Value/weight
+Arrays.sort(arr,(a,b)->{
+returnDouble.compare((double)(b.value)/(double)(b.weight), (double)(a.value)/(double)(a.weight));
+});
+ 
+intcurrWeight=0;
+doublecurrValue=0;
+ 
+//traverse the array
+for(int i=0; i<n; i++){
+ 
+if(currWeight+arr[i].weight<=W){
+currWeight+=arr[i].weight;
+currValue+=arr[i].value;
+ 
+}else{
+int remaining=W-currWeight;
+currValue+= (double)(arr[i].value)/(double)(arr[i].weight) * (double)remaining;
+break;
 }
-// } Driver Code Ends
+}
+ 
+returncurrValue;
+}
+}
+
+ 
+
+
